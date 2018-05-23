03cd441e1e999a2621b95576482f14ab9c7b6837
ginogueira11 <39565335+ginogueira11@users.noreply.github.com>
2018-05-23 13:42:37 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercício4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            int count0_25 = 0, count26_50 = 0, count51_75 = 0, count76_100 = 0;
+            do
+            {
+                Console.WriteLine("Digite um número:");
+                num = Convert.ToInt16(Console.ReadLine());
+
+                if (num >= 0 && num <=25 )
+                {
+                    count0_25 = count0_25 + 1; 
+                }else if (num >= 26 && num <=50)
+                {
+                    count26_50++;
+                }else if (num >= 51 && num <= 75)
+                {
+                    count51_75++;
+                }else if (num >= 76 && num <= 100)
+                {
+                    count76_100++;
+                }
+
+            } while (num != 0);
+
+            Console.WriteLine("count0_25" + count0_25);
+            Console.WriteLine("count26_50" + count26_50);
+            Console.WriteLine("count51_75" + count51_75);
+            Console.WriteLine("count76_100" + count76_100);
+        }
+    }
+}
---
+++ exercício1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercício1
+{
+    class exercício1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite 10 número INTEIROS:");
+            int num = Convert.ToInt16(Console.ReadLine());
+
+            if (num % 2 == 1)
+            {
+                Console.WriteLine("{0}", num);
+            }
+            else if (num % 2 == 0)
+            {
+                Console.WriteLine("{0}", num);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
