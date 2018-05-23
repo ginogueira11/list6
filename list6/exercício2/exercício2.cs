ee99295ecbd648faa6a57e2059ec6124de062b0e
ginogueira11 <39565335+ginogueira11@users.noreply.github.com>
2018-05-23 14:06:23 -0300

    Add files via upload

---
+++ list6/exercício2/exercício2.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercício2
+{
+    class exercício2
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            for (n = 1; n <= 1000; n++)
+            {
+                Console.WriteLine("{0}",n);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
